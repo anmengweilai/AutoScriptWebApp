1d452f4a680ed7a28f702491f50b4866395d7013
lizhihong <zhihong.li@petkit.com>
2024-01-17 18:56:21 +0800

    feat(app): auto update

--- packages/main/src/utils/AutoUpdater.ts
+++ packages/main/src/utils/AutoUpdater.ts
@@ -1,5 +1,9 @@
 import { autoUpdater } from 'electron-updater';
 
+/**
+ * 还未实现考虑参考？
+ * TODO：https://juejin.cn/post/7054811432714108936
+ */
 export class AppUpdater {
   constructor() {
     autoUpdater.checkForUpdatesAndNotify();
